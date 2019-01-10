45a9c0ab830c6ecd87ee9b794ebd1e886753d34c
ozzy <ozanaltiniku@gmail.com>
2019-01-10 14:02:12 -0800

    4.3 Menus and Pickers

--- app/src/main/java/com/example/altin/droidcafe/OrderActivity.java
+++ app/src/main/java/com/example/altin/droidcafe/OrderActivity.java
@@ -2,14 +2,23 @@ package com.example.altin.droidcafe;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
- * This activity shows the order chosen.  The order is sent as data
- * with the intent to launch this activity.
+ * This activity handles radio buttons for choosing a delivery method for an
+ * order, and EditText input controls.
  */
 public class OrderActivity extends AppCompatActivity {
 
+    /**
+     * Sets the content view to activity_order, and gets the intent and its
+     * data.
+     *
+     * @param savedInstanceState Saved instance state bundle.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +29,47 @@ public class OrderActivity extends AppCompatActivity {
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView textView = findViewById(R.id.order_textview);
         textView.setText(message);
+    }
+
+    /**
+     * Checks which radio button was clicked and displays a toast message to
+     * show the choice.
+     *
+     * @param view The radio button view.
+     */
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+        // Check which radio button was clicked.
+        switch (view.getId()) {
+            case R.id.sameday:
+                if (checked)
+                    // Same day service
+                    displayToast(getString(R.string.same_day_messenger_service));
+                break;
+            case R.id.nextday:
+                if (checked)
+                    // Next day delivery
+                    displayToast(getString(R.string.next_day_ground_delivery));
+                break;
+            case R.id.pickup:
+                if (checked)
+                    // Pick up
+                    displayToast(getString(R.string.pick_up));
+                break;
+            default:
+                // Do nothing.
+                break;
+        }
+    }
+
+    /**
+     * Displays the actual message in a toast message.
+     *
+     * @param message Message to display.
+     */
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message,
+                Toast.LENGTH_SHORT).show();
     }
 }
